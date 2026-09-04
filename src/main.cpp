73c141c23c5587635e3166a3ba8ffa4c66b1d0a2
Christopher <github.android206@passmail.net>
2026-09-04 20:59:26 +0200

    Add SHT45 readout

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,32 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_SHT4x.h>
+
+Adafruit_SHT4x sht45;
 
 void setup() {
     Serial.begin(115200);
-    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.setTxTimeoutMs(0);
+    delay(2000);
+
+    pinMode(I2C_POWER, OUTPUT);
+    digitalWrite(I2C_POWER, HIGH);
+    delay(100);
+
+    Wire.begin();
+
+    if (!sht45.begin(&Wire)) {
+        Serial.println("SHT45 not found");
+        while (true) delay(1000);
+    }
+    sht45.setPrecision(SHT4X_HIGH_PRECISION);
+    sht45.setHeater(SHT4X_NO_HEATER);
+    Serial.printf("SHT45 serial: %08lx\n", sht45.readSerial());
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("on");
-    delay(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.println("off");
-    delay(500);
+    sensors_event_t hum, temp;
+    sht45.getEvent(&hum, &temp);
+    Serial.printf("T %.2f C  RH %.2f %%\n", temp.temperature, hum.relative_humidity);
+    delay(2000);
 }
